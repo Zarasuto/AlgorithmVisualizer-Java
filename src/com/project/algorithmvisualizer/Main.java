0efaf496e8ffe1cefc0afbe90ca72cefeac41be4
zarasuto <zarasuto@gmail.com>
2021-06-10 21:59:49 +0800

    Created visualizer with shuffle. (RE)
    I don't know why when i pushed it, it doesn't have the code. so double push

--- src/com/project/algorithmvisualizer/Main.java
+++ src/com/project/algorithmvisualizer/Main.java
@@ -1,19 +1,35 @@
 package com.project.algorithmvisualizer;
 
 import javax.swing.JFrame;
+import javax.swing.JComponent;
+import java.awt.*;
+
 public class Main {
-    public static final int WIN_WIDTH=1280;
+    public static final int WIN_WIDTH= 1280;
     public static final int WIN_HEIGHT=720;
+    private SortVisualizer sortVisualizer;
 
     private JFrame window;
 
     public Main(){
+
+        //NOT YET OPTIMIZED
         window = new JFrame("Algorithm Visualizer");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(WIN_WIDTH,WIN_HEIGHT);
+        //window.setResizable(false);
+        sortVisualizer = new SortVisualizer();
+        window.getContentPane().add(sortVisualizer);
+        window.pack();
         window.setVisible(true);
+        sortVisualizer.shuffle();
+        sortVisualizer.resetColors();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Main main = new Main();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/project/algorithmvisualizer/SortVisualizer.java
+++ src/com/project/algorithmvisualizer/SortVisualizer.java
@@ -1,2 +1,103 @@
-package com.project.algorithmvisualizer;public class SortVisualizer {
+package com.project.algorithmvisualizer;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Random;
+import javax.swing.JPanel;
+
+public class SortVisualizer extends JPanel{
+
+    private int arrayChanges; //counts the number of changes in the array
+
+    private final int BAR_WIDTH=5;
+    private final int BAR_NUMS = Main.WIN_WIDTH/BAR_WIDTH;
+
+    private int[] arr;
+    private int[] barColor;
+
+    public SortVisualizer(){
+        setBackground(Color.black);
+        arr = new int[BAR_NUMS];
+        barColor = new int[BAR_NUMS];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i+1;
+            barColor[i]=0;
+        }
+    }
+
+    private void finaliseUpdate(long millisecondDelay, boolean isChanged) {
+        repaint();
+        try {
+            Thread.sleep(millisecondDelay);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        if (isChanged)
+            arrayChanges++;
+    }
+
+    public void swap(int index1, int index2,long millisecondDelay, boolean isChanged){
+        int temp = arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+
+        barColor[index1]=120;
+        barColor[index2]=120;
+
+        finaliseUpdate(millisecondDelay,isChanged);
+    }
+    public void shuffle() {
+        Random rng = new Random();
+        for (int i = 0; i < arraySize(); i++) {
+            int swapWithIndex = rng.nextInt(arraySize() - 1);
+            swap(i, swapWithIndex, 2,false);
+        }
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        Graphics2D graphics = (Graphics2D)g;
+        super.paintComponent(graphics);
+        for(int i=0;i<BAR_NUMS;i++){
+
+            int barheight=arr[i]*2;
+            int xbegin =((BAR_WIDTH)*(i));
+            int ybegin= Main.WIN_HEIGHT-barheight;
+            int val=barColor[i]*2;
+            if(val>220){
+                graphics.setColor(new Color(255-val,255,255-val));
+            }else{
+                graphics.setColor(new Color(255,255-val,255-val));
+            }
+            graphics.fillRect(xbegin,ybegin,BAR_WIDTH,barheight);
+            if(barColor[i]>0){
+                barColor[i]-=10;
+            }
+        }
+    }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(Main.WIN_WIDTH,Main.WIN_HEIGHT);
+    }
+    public void resetColors() {
+        for (int i = 0; i < BAR_NUMS; i++) {
+            barColor[i] = 0;
+        }
+        repaint();
+    }
+    public int arraySize() {
+        return arr.length;
+    }
+
+    public int getValue(int index) {
+        return arr[index];
+    }
+    /**
+     * Gets the max value of the array or Integer.MIN_VALUE if there isn't one.
+     * @return the max value or Integer.MIN_VALUE.
+     */
+    public int getMaxValue() {
+        return Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+    }
 }
