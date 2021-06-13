b81561c47b13aca22623d930e87b81c2e5a5508e
zarasuto <zarasuto@gmail.com>
2021-06-13 16:49:26 +0800

    Added Stack Visualization Screen

---
+++ src/com/project/algorithmvisualizer/screen/StackVisualizerScreen.java
@@ -0,0 +1,28 @@
+package com.project.algorithmvisualizer.data_structures;
+
+import com.project.algorithmvisualizer.screen.MainScreen;
+import com.project.algorithmvisualizer.screen.Screen;
+
+import javax.swing.*;
+
+public class StackVisualizer extends Screen {
+
+    public StackVisualizer(MainScreen app) {
+        super(app);
+    }
+
+    @Override
+    public void startOperation() {
+
+    }
+
+    @Override
+    public void backToMenu() {
+
+    }
+
+    @Override
+    public String getScreenName() {
+        return "Stack";
+    }
+}
